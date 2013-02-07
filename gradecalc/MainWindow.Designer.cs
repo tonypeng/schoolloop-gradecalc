82a9864aa8e4ebf1cbed9f7afdbec76edadbc9cc
tonypeng1337 <tony.peng1337@gmail.com>
2013-02-07 00:18:55 -0800

    Code import

---
+++ gradecalc/MainWindow.Designer.cs
@@ -0,0 +1,87 @@
+﻿namespace gradecalc
+{
+    partial class MainWindow
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(631, 356);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "Go to your progress report for any course, press CTRL+A, and paste into this box." +
+    "";
+            this.richTextBox1.TextChanged += new System.EventHandler(this.richTextBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 375);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(42, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Output:";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(12, 392);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.ReadOnly = true;
+            this.richTextBox2.Size = new System.Drawing.Size(631, 226);
+            this.richTextBox2.TabIndex = 2;
+            this.richTextBox2.Text = "";
+            // 
+            // MainWindow
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(655, 630);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Name = "MainWindow";
+            this.Text = "gradecalc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+    }
+}
+
---
+++ gradecalc/MainWindow.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace gradecalc
+{
+    public partial class MainWindow : Form
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            richTextBox1.SelectAll();
+        }
+
+        private void richTextBox1_TextChanged(object sender, EventArgs e)
+        {
+            richTextBox2.Clear();
+
+            //try
+            //{
+                string[] parts = richTextBox1.Text.Split(new string[] { "SCORE PER CATEGORY" }, StringSplitOptions.RemoveEmptyEntries);
+
+                if (parts.Length != 2)
+                    return;
+
+                // parts 2 should be the weight table
+                Dictionary<string, float> weightMap = new Dictionary<string, float>();
+
+                string[] weights = parts[1].Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                bool isweighted = true;
+
+                foreach (string str in weights)
+                {
+                    if (str == "GRADE SCALE" || str == "Click here for an explanation of weighting.")
+                        break;
+
+                    string[] details = str.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (details.Length == 3)
+                    {
+                        details[1] = details[1].Substring(0, details[1].Length - 1);
+
+                        float weight;
+
+                        if (float.TryParse(details[1], out weight))
+                        {
+                            weightMap.Add(details[0], weight / 100f);
+                        }
+                    }
+                    else
+                    {
+                        isweighted = false;
+                        weightMap.Add("Total Grade", 1.0f);
+                        break;
+                    }
+                }
+
+                string[] grades = parts[0].Split(new string[] { "Submissions:" }, StringSplitOptions.RemoveEmptyEntries);
+
+                grades[1] = grades[1].Trim();
+
+                string[] assignments = grades[1].Split(new char[] { '\n' }, StringSplitOptions.RemoveEmptyEntries);
+
+                Dictionary<string, float> bases = new Dictionary<string, float>();
+                Dictionary<string, float> scores = new Dictionary<string, float>();
+
+                float totalWeight = 0f;
+
+                foreach (string assignment in assignments)
+                {
+                    string[] assignment_parts = assignment.Split(new char[] { '\t' }, StringSplitOptions.RemoveEmptyEntries);
+                    
+                    if (assignment_parts.Length < 5)
+                        continue; 
+                    
+                    string weight_type = !isweighted ? "Total Grade" : assignment_parts[0].Trim();
+
+                    string[] score_parts = assignment_parts[4].Trim().Split(new string[] { " = " }, StringSplitOptions.RemoveEmptyEntries);
+
+                    string[] num_den = score_parts[0].Trim().Split(new string[] { " / " }, StringSplitOptions.RemoveEmptyEntries);
+
+                    if (!bases.ContainsKey(weight_type))
+                    {
+                        bases.Add(weight_type, 0f);
+                        totalWeight += weightMap[weight_type];
+                    }
+
+                    bases[weight_type] += float.Parse(num_den[1]);
+
+                    if (!scores.ContainsKey(weight_type))
+                        scores.Add(weight_type, 0f);
+
+                    scores[weight_type] += float.Parse(num_den[0]);
+                }
+
+                float finalScore = 0f;
+
+                foreach (KeyValuePair<string, float> kvp in weightMap)
+                {
+                    if (scores.ContainsKey(kvp.Key))
+                    {
+                        finalScore += kvp.Value * scores[kvp.Key] / bases[kvp.Key];
+
+                        writeline(kvp.Key + ": " + scores[kvp.Key] + " / " + bases[kvp.Key]);
+                    }
+                    else
+                    {
+                        writeline(kvp.Key + ": 0 / 0");
+                    }
+                }
+
+                finalScore /= totalWeight;
+
+                writeline(finalScore.ToString());
+            //}
+            //catch (Exception ex)
+            //{
+            //    writeline("error parsing text: " + ex.Message);
+            //}
+        }
+
+        private void writeline(string str)
+        {
+            richTextBox2.Text += str + "\n";
+        }
+    }
+}
